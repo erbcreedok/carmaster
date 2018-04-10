b3552e5a248c867475b0686a14d2d75752857cc9
Yerbol Syzdyk <erbcreedok@gmail.com>
2018-04-10 12:13:58 +0600

    Change method of thanks

--- assets/js/main.js
+++ assets/js/main.js
@@ -14578,6 +14578,7 @@ $( document ).ready(function() {
                     $(form.elements).attr('disabled', false);
                     $(form.elements).val('');
                     $('.ajax-status').html('Отправить');
+                    $('#callbackModal').modal('show');
                     $('.thanks-fade-in').fadeIn(function () {
                         setTimeout(function () {
                             $('#callbackModal').modal('hide');
